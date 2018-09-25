0f178de6ffa8a3eb8c544c260a6cecc9fb632bd9
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-09-25 13:38:40 -0500

    Added in Sample Scene

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    public RigidBody2D myRigidbody;
+
+	void Start ()
+    {
+        //How to print to console
+        //Debug.Log("This is Start!");
+	}
+	
+
+	void Update ()
+    {
+        if (Input.GetKey(left))
+        {
+            myRigidbody.velocity = new Vector2(-5, myRigidbody.velocity.y);
+        }
+        else if (Input.GetKey(right))
+        {
+            myRigidbody.velocity = new Vector2(5, myRigidbody.velocity.y);
+        }
+	}
+}
