4c3857e09c4c3f826b298304095479f722bcbca8
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-10-23 14:45:44 -0500

    Added Breakable Blocks and Basic Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+
+    [SerializeField]
+    private float speed = 10;
+
+    private float shotDirection;
+
+    PlayerMovement player = collision.GetComponent<PlayerMovement>();
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    { 
+
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+
+            player.Respawn();
+        }
+        else
+        {
+
+        }
+    }
+}
