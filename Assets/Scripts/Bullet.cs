bf288e77a7e8c712afeccc7f93f87a517346c34a
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-11-27 13:53:55 -0600

    Re-adding scripts folder for code review

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody2D myRigidBody;
+
+    private float shotDirection;
+
+    public static List<Bullet> allBullets = new List<Bullet>();
+
+    private void Awake()
+    {
+        allBullets.Add(this);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            PlayerController player = collision.GetComponent<PlayerController>();
+            player.Respawn();
+        }
+    }
+}
---
+++ Assets/Scripts/Collectible.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible : MonoBehaviour
+{
+    private AudioSource activatedSound;
+    private SpriteRenderer spriteRenderer;
+    private BoxCollider2D boxCollider2D;
+
+    private void Start()
+    {
+        activatedSound = GetComponent<AudioSource>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        boxCollider2D = GetComponent<BoxCollider2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            activatedSound.Play();
+            boxCollider2D.enabled = false;
+            spriteRenderer.enabled = false;
+            Destroy(gameObject,activatedSound.clip.length);
+        }
+    }
+}
+
