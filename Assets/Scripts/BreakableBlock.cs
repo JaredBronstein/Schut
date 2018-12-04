c2f09b7a36977327fc779a1b38ffe4249dbad56d
JaredBronstein <43156735+JaredBronstein@users.noreply.github.com>
2018-12-04 15:10:11 -0600

    Added Level 2, 3 and re-added breakable block animation

--- Assets/Scripts/BreakableBlock.cs
+++ Assets/Scripts/BreakableBlock.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class BreakableBlock : MonoBehaviour
 {
+    [SerializeField]
+    private Animator anim;
+
+    private BoxCollider2D boxCollider;
+
+    private void Start()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider2D>();
+    }
     /// <summary>
-    /// Destroys self when in contact with a bullet
+    /// Destroys self after playing an animation when in contact with a bullet
     /// </summary>
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Bullet"))
         {
-            Destroy(this.gameObject);
+            anim.SetBool("IsBroken", true);
+            boxCollider.enabled = false;
+            Invoke("Destroy", 0.1f);
         }
+    }
+    private void Destroy()
+    {
+        Destroy(this.gameObject);
     }
 }
